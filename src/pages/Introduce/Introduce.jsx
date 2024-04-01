12c829b31c62bd481f96dea99a5c1edbebbaa718
zafergunayonalan <zafergunay7@gmail.com>
2024-04-02 01:19:04 +0300

    ıntroduce-edited

--- src/pages/Introduce/Introduce.jsx
+++ src/pages/Introduce/Introduce.jsx
@@ -1,10 +1,17 @@
+import Dropdown from "../../components/dropdown/Dropdown"
 import "./Introduce.css"
 
 const Introduce = () => {
+  const difficulty = ["easy" , "medium" , "hard"]
+
   return (
     <div className='introduce'>
       <div className="introduce-container">
-        <img src="" alt="" />
+        <img src="https://thetrainingarcade.com/wp-content/uploads/2020/06/Trivia-logo-01-1024x473.png" alt="" />
+        <Dropdown/>
+        <div className="introduce-btn">
+          Quiz'e basla
+        </div>
       </div>
     </div>
   )
