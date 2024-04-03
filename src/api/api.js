412cc639989ea3c08d1b5dc8ea8256f1cf4fac86
zafergunayonalan <zafergunay7@gmail.com>
2024-04-03 23:37:03 +0300

    api çekildi

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,14 @@
 
 const shuffleArray = (array) => {
-    return [...array].sort(() => Math.random - 0.5)
+    return [...array].sort(() => Math.random() - 0.5)
 }
 
 
 export const fetchQuizData = async(difficulty,amount) => {
     const url = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
-    const data = await(await fetch(url)).json();
-    console.log(data)
+    const data = await (await fetch(url)).json();
     return data.results.map((dt) => ({
         ...dt,
-        answer: shuffleArray([...dt.incorrect_answer, dt.correct_answer])
+        answers: shuffleArray([...dt.incorrect_answers, dt.correct_answer])
     }))
 }
--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import * as api from "../../api/api"
 import "./Quiz.css"
 
 const Quiz = () => {
   const {difficulty , amount} = useParams()
+  const [questionsData, setQuestionsData] = useState([])
+
+  useEffect(() => {
+    const getData = async() => {
+      const data = await api.fetchQuizData(difficulty,amount)
+      setQuestionsData(data)
+    }
+    getData();
+  }, [])
+  
+  console.log(questionsData, "questionsData")
+  
   return (
     <div className='quiz'>
       {difficulty} - {amount}
