32d4f3c995a938e135f4c881a8d046393f7d796e
zafergunayonalan <zafergunay7@gmail.com>
2024-04-06 23:01:33 +0300

    .

--- src/components/questions/QuestionCard.jsx
+++ src/components/questions/QuestionCard.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import "./QuestionCard.css"
 
 const QuestionCard = ({questionsData, score, setScore, count, setCount, modal, setModal}) => {
+
+    const approvedChoice = (e) => {         
+        console.log(e.currentTarget.value);
+    }
+
   return (
     <div className='questionCard'>
         <div>{count + 1}/10 - {questionsData[count]?.question}</div>
+        {
+            questionsData[count]?.answers?.map((answer, i) =>(
+                <button onClick={approvedChoice} value={answer} key={i} >{answer}</button>
+            ))
+        }
     </div>
   )
 }
